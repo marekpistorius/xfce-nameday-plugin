6a2b0ffde3bbc4f316806366c259e68540d897a9
Marek Pistorius <marek.pistorius@mpistorius.eu>
2022-04-26 13:45:22 +0200

    Update nameday-definitions.h
    Update Website URL

--- panel-plugin/nameday-definitions.h
+++ panel-plugin/nameday-definitions.h
@@ -1,5 +1,5 @@
 #define NAMEDAY_PATH_GLOBAL "/usr/share/xfce4/nameday/"
 /* the website url */
-#define PLUGIN_WEBSITE "https://mpistorius.eu"
+#define PLUGIN_WEBSITE "https://marekpistorius.github.io/xfce-nameday-plugin/"
 
 //#define VERSION "1.0.2"
