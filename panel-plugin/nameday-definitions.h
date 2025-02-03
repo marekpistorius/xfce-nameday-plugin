032850a276525792602554c8aa5246d2ab189fed
Marek Pistorius <me@maniu.eu>
2025-02-03 17:39:02 +0100

    Add proper GPL + Copyright header to all files

--- panel-plugin/nameday-definitions.h
+++ panel-plugin/nameday-definitions.h
@@ -1,3 +1,13 @@
+/* 
+* (C) 2024-2025 Marek Pistorius
+* This file is part of xfce-nameday-plugin.
+* xfce-nameday-plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+*
+* xfce-nameday-plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with Xfce-nameday-plugin. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #define NAMEDAY_PATH_GLOBAL "/usr/share/xfce4/nameday/"
 /* the website url */
 #define PLUGIN_WEBSITE "https://marekpistorius.github.io/xfce-nameday-plugin/"
