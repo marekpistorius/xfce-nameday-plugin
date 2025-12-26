ac226fd99a9d6a7640f08f14ef762deda3e7d6d4
Marek Pistorius <me@maniu.eu>
2025-12-26 16:27:00 +0100

    Update nameday-plugin-callendar.c
    Signed-off-by: Marek Pistorius <me@maniu.eu>

--- panel-plugin/nameday-plugin-callendar.c
+++ panel-plugin/nameday-plugin-callendar.c
@@ -1,5 +1,5 @@
 /* 
-* (C) 2024-2025 Marek Pistorius
+* (C) 2024-2026 Marek Pistorius
 * This file is part of xfce-nameday-plugin.
 * xfce-nameday-plugin is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 *
